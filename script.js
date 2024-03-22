336aa59fdf6d3e37796c931670d36612b97209e9
Henning Summer <115693958+henning-nr@users.noreply.github.com>
2024-03-22 22:50:57 +0000

    adicionando barra de navegação e melhorando layout

--- script.js
+++ script.js
@@ -1,25 +1,66 @@
+// Inicializando javascript do materialize
+$(document).ready(function(){
+	$('.tabs').tabs();
+	$('.modal').modal();
+	$('.sidenav').sidenav();
+});
 
-function buscaCep() {
-	const cep = document.getElementById('cep').value
-	const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+let url = "" // Declarando variavel que vai armazenar a URL montada
+
+// Função que vai buscar o cep ou a rua conforme o tipo passado na função
+function buscaCep(tipo) {
+	console.log('tipo: ', tipo)
+	if(tipo == "cep"){
+		const cep = $('#cep').val()
+		url = `https://viacep.com.br/ws/${cep}/json/`;
+	}else{
+		const uf = $('#uf').val() // USANDO JQUERY $ PARA PEGAR VALOR DA UF
+		const cidade = $('#cidade').val() // USANDO JQUERY $ PARA PEGAR VALOR DA CIDADE
+		const rua = $('#rua').val() // JS PURO VANILLA PEGAR RUA
+		url = `https://viacep.com.br/ws/${uf}/${cidade}/${rua}/json/`
+	}
+
 	fetch(url)
 		.then(response => {
 			return response.json();
 		})
 		.then(data => {
 			console.log(data);
-			document.getElementById('logradouro').value = data.logradouro;
+			if(tipo == "cep"){
+				let li = ""
+				li += `<li class="collection-item">CEP: ${data.cep}</li>`
+				li += `<li class="collection-item">Logradouro: ${data.logradouro}</li>`
+				li += `<li class="collection-item">Bairro: ${data.bairro}</li>`
+				li += `<li class="collection-item">Localidade: ${data.localidade}</li>`
+			$("#resultado").html(li)
+			}else{
+				let lis = ""
+				for(let rua of data){
+					lis += `<li class="collection-item">${rua.logradouro}</li>`
+					lis += `<li class="collection-item">${rua.bairro}</li>`
+					lis += `<li class="collection-item">${rua.localidade}</li>`
+					lis +='<br>'
+					lis += '<hr>'
+				}
+			$("#resultado").html(lis) // JQUERY
+			document.getElementById("resultado").innerHTML = lis // JS PURO VANILLA
+			}
 
-			document.getElementById('bairro').value = data.bairro;
+			
 		})
 }
 
 
-document.querySelector("button").addEventListener("mouseout", (e) => {
-	console.log('passei',e)
+document.querySelector("#form-cep").addEventListener("submit", (e) => {
+	e.preventDefault()
+	console.log('submit',e.target)
+	const formData = new FormData(e.target);
+	const data = Object.fromEntries(formData.entries());
+	alert(JSON.stringify(data));
 })
 
-document.querySelector("form").addEventListener("submit", (e) => {
+document.querySelector("#form-rua").addEventListener("submit", (e) => {
 	e.preventDefault()
 	console.log('submit',e.target)
 	const formData = new FormData(e.target);
