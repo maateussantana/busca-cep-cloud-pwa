02ab963a629f228261aeeea26a5b22847803a856
Henning Summer <115693958+henning-nr@users.noreply.github.com>
2024-04-16 08:13:16 +0000

    feat(add-taks-readme): add task readme about project in the class activity

---
+++ readme.md
@@ -0,0 +1,41 @@
+### ATIVIDADE PARA A PROVA
+
+1. - IMPLEMENTAR O LOG - ok
+2. - ADICIONAR UM LOADING PARA PESQUISA - ok
+3. - TRANSFORMAR EM UM PWA - falta
+4. - COLOCAR NA "NUVEM" DA W3SCHOOL - falta
+
+### Entregar até o dia da PROVA!!!
+
+
+
+## O QUE FAZER?
+
+# Progressive Web Apps (PWAs)
+
+Um PWA (Progressive Web App) é um tipo de aplicativo web que utiliza tecnologias modernas para oferecer uma experiência de usuário semelhante à de um aplicativo nativo. Eles são construídos com tecnologias web padrão, como HTML, CSS e JavaScript, mas têm recursos adicionais que os aproximam das funcionalidades de um aplicativo nativo.
+
+## Características de um PWA
+
+1. **Responsividade**: Os PWAs são projetados para funcionar em qualquer dispositivo, seja desktop, tablet ou smartphone.
+
+2. **Confiabilidade**: Eles são capazes de funcionar offline ou em redes de baixa qualidade, graças ao armazenamento em cache e ao uso de Service Workers.
+
+3. **Rapidez**: PWAs carregam rapidamente e oferecem uma experiência de usuário fluida, semelhante à de um aplicativo nativo.
+
+4. **Engajamento**: PWAs podem enviar notificações push para usuários, incentivando o engajamento e a retenção.
+
+5. **Instalabilidade**: Eles podem ser "instalados" diretamente no dispositivo do usuário, aparecendo na tela inicial e oferecendo uma experiência de inicialização semelhante à de um aplicativo nativo.
+
+## Vantagens dos PWAs
+
+- **Acessibilidade**: PWAs são acessíveis através de um navegador web, eliminando a necessidade de download e instalação através de uma loja de aplicativos.
+
+- **Compatibilidade**: Eles são compatíveis com a maioria dos navegadores modernos, reduzindo a necessidade de desenvolvimento específico para plataformas diferentes.
+
+- **Atualização fácil**: As atualizações de PWAs são feitas automaticamente assim que o usuário abre o aplicativo em seu navegador, eliminando a necessidade de atualizações manuais.
+
+Em resumo, os PWAs combinam o melhor dos dois mundos: a acessibilidade e a facilidade de desenvolvimento da web com a funcionalidade e a experiência do usuário de um aplicativo nativo. Isso os torna uma escolha atraente para muitas empresas que desejam alcançar um público amplo com uma experiência de usuário de alta qualidade.
+
+### NUVEM DA W3SCHOOLS
+#### Após implementar as mudanças solicitadas, migrar o projeto do replit para o space dentro do W3School colocando um nome adquequado para o projeto que será o mesmo usado na URL do seu site. https://spaces.w3schools.com/
--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 // Inicializando javascript do materialize
-$(document).ready(function(){
+$(document).ready(function() {
 	$('.tabs').tabs();
 	$('.modal').modal();
 	$('.sidenav').sidenav();
+	$('.progress').hide()
 });
 
+// Aplicando mascara
+$('#cep').inputmask("99999-999");
 
 let url = "" // Declarando variavel que vai armazenar a URL montada
 
 // Função que vai buscar o cep ou a rua conforme o tipo passado na função
 function buscaCep(tipo) {
+	$('.progress').show()
 	console.log('tipo: ', tipo)
-	if(tipo == "cep"){
+	if (tipo == "cep") {
 		const cep = $('#cep').val()
+		let log = {
+			date: getDate(),
+			log: cep
+		}
+		saveLog(log)
 		url = `https://viacep.com.br/ws/${cep}/json/`;
-	}else{
-		const uf = $('#uf').val() // USANDO JQUERY $ PARA PEGAR VALOR DA UF
-		const cidade = $('#cidade').val() // USANDO JQUERY $ PARA PEGAR VALOR DA CIDADE
+	} else {
+		const uf = $('#ufs-list').val() // USANDO JQUERY $ PARA PEGAR VALOR DA UF
+		const cidade = $('#cidades-list').val() // USANDO JQUERY $ PARA PEGAR VALOR DA CIDADE
 		const rua = $('#rua').val() // JS PURO VANILLA PEGAR RUA
+
+		let log = {
+			date: getDate(),
+			log: uf + '-' + cidade + '-' + rua,
+		}
+		saveLog(log)
 		url = `https://viacep.com.br/ws/${uf}/${cidade}/${rua}/json/`
 	}
 
@@ -27,43 +42,132 @@ function buscaCep(tipo) {
 		})
 		.then(data => {
 			console.log(data);
-			if(tipo == "cep"){
+			if (tipo == "cep") {
 				let li = ""
-				li += `<li class="collection-item">CEP: ${data.cep}</li>`
-				li += `<li class="collection-item">Logradouro: ${data.logradouro}</li>`
-				li += `<li class="collection-item">Bairro: ${data.bairro}</li>`
-				li += `<li class="collection-item">Localidade: ${data.localidade}</li>`
-			$("#resultado").html(li)
-			}else{
+				li += `<li class="collection-item adjust-list"><i class="material-icons">streetview</i> ${data.logradouro}</li>`
+				li += `<li class="collection-item adjust-list"><i class="material-icons">location_on</i> ${data.bairro}</li>`
+				li += `<li class="collection-item adjust-list"><i class="material-icons">location_city</i> ${data.localidade}</li>`
+				setTimeout(() => {
+					$('.progress').hide()
+					$("#resultado").html(li)
+				}, 1000)
+			} else {
 				let lis = ""
-				for(let rua of data){
-					lis += `<li class="collection-item">${rua.logradouro}</li>`
-					lis += `<li class="collection-item">${rua.bairro}</li>`
-					lis += `<li class="collection-item">${rua.localidade}</li>`
-					lis +='<br>'
-					lis += '<hr>'
+				for (let rua of data) {
+					lis += `<li class="collection-item adjust-list"><i class="material-icons">thumb_up</i> ${rua.cep}</li>`
+					lis += `<li class="collection-item adjust-list"><i class="material-icons">streetview</i> ${rua.logradouro}</li>`
+					lis += `<li class="collection-item adjust-list"><i class="material-icons">location_on</i> ${rua.bairro}</li>`
+					lis += `<li class="collection-item adjust-list"><i class="material-icons">location_city</i> ${rua.localidade}</li>`
+					lis += `<li><span style="width: 100%;display: flex;justify-content: center;">● ● ●</span></li>`
 				}
-			$("#resultado").html(lis) // JQUERY
-			document.getElementById("resultado").innerHTML = lis // JS PURO VANILLA
+				setTimeout(() => {
+					$('.progress').hide()
+					$("#resultado").html(lis) // JQUERY
+				}, 1000)
 			}
-
-			
 		})
+}
+
+function saveLog(log) {
+	let logs = JSON.parse(localStorage.getItem('logs'))
+	if (logs == null) {
+		logs = []
+	}
+	logs.push(log)
+	localStorage.setItem('logs', JSON.stringify(logs))
+}
+
+function mountLog() {
+	let logs = JSON.parse(localStorage.getItem('logs'))
+	let logsList = ""
+	for (let log of logs) {
+		logsList += `<li class="collection-item">${log.date} buscou ${log.log}</li>`
+	}
+	$("#resultado").html(logsList)
+}
+
+function getDate() {
+	const dataAtual = new Date();
+	const options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		second: 'numeric'
+	};
+	const dataFormatada = dataAtual.toLocaleString('pt-BR', options);
+	return dataFormatada
 }
 
 
 document.querySelector("#form-cep").addEventListener("submit", (e) => {
 	e.preventDefault()
-	console.log('submit',e.target)
+	console.log('submit', e.target)
 	const formData = new FormData(e.target);
 	const data = Object.fromEntries(formData.entries());
-	alert(JSON.stringify(data));
+	// alert(JSON.stringify(data));
 })
 
 document.querySelector("#form-rua").addEventListener("submit", (e) => {
 	e.preventDefault()
-	console.log('submit',e.target)
+	console.log('submit', e.target)
 	const formData = new FormData(e.target);
 	const data = Object.fromEntries(formData.entries());
-	alert(JSON.stringify(data));
+	// alert(JSON.stringify(data));
 })
+
+async function pegarUfs() {
+	await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+		.then((res) => { return res.json() })
+		.then((ufs) => {
+			console.log(ufs)
+			let ufList = $("#ufs-list") // peguei o select pra inserir options
+			let ufOptions = '<option value="0" disabled selected>Escolha um estado</option>'
+			// ler todos estados
+			for (let uf of ufs) {
+				ufOptions += `<option value="${uf.sigla}">${uf.nome}</option>`
+			}
+
+			ufList.html(ufOptions)
+			$('select').formSelect();
+		})
+}
+
+function pegarCidades() {
+	let uf = $("#ufs-list").val()
+	console.log("uf aqui", uf)
+	getCidades(uf)
+}
+
+function getCidades(uf) {
+	fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + uf + "/municipios")
+		.then((res) => { return res.json() })
+		.then((cidades) => {
+			console.log(cidades)
+			let cidadesList = $("#cidades-list") // peguei o select pra inserir options
+			let cidadesOptions = '<option value="0" disabled selected>Escolha uma cidade</option>'
+			// ler todos estados
+			for (let cidade of cidades) {
+				cidadesOptions += `<option value="${cidade.nome}">${cidade.nome}</option>`
+			}
+
+			cidadesList.html(cidadesOptions)
+			$('select').formSelect();
+		})
+}
+
+function clearTab() {
+	console.log("limpando")
+	$("#resultado").html("")
+	$("#cep").val('')
+
+	pegarUfs()
+	$("#cidades-list").html('')
+	$("#rua").val('')
+}
+
+pegarUfs()
+
+
